f06419842772bb1b22950e07f2743eb689bb5e11
sancdd01 <derricksanchez@ymail.com>
2024-08-01 17:50:31 -0700

    modifications for mediascreens

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
-      <p>
-        <a href="#home">Home</a>
-      </p>
-      <p>
-        <a href="#about">About Me</a>
-      </p>
-      <p>
-        <a href="#projects">Projects</a>
-      </p>
-      <p>
-        <a href="#contact">Contact</a>
-      </p>
+      <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <p><a href="#home">Home</a></p>
+
+        <p><a href="#about">About Me</a></p>
+
+        <p><a href="#projects">Projects</a></p>
+
+        <p><a href="#contact">Contact</a></p>
+
+      </div>
+      <div className="navbar-hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
     </div>
   );
 };
